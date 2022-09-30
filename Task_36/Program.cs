bbd5fdd3937101cd8ee98a9678c019757d1236ab
Vasiliy <shumchenko.94@mail.ru>
2022-09-30 22:00:31 +0300

    Sum of elements

---
+++ Task_36/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+Console.Write("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] number = new int[size];
+int i = 0;
+int summa = 0;
+CreatArray(number);
+PrintArray(number);
+
+for(i = 1; i < number.Length; i+=2)
+{
+    summa = summa + number[i];
+    Console.Write(number[i] + " ");
+}
+Console.WriteLine();
+Console.WriteLine($"Сумма равна: {summa}");
+
+
+void CreatArray(int [] number)
+{
+    
+    for (int i = 0; i < number.Length; i++)
+    {
+        number[i] = new Random().Next(1, 10);
+    }
+}
+void PrintArray(int[] arrayCreat)
+{
+    Console.Write("Массив: [");
+    for (int i = 0; i < arrayCreat.Length - 1; i++)
+    {
+        Console.Write(arrayCreat[i] + ", ");
+    }
+    Console.Write(arrayCreat[arrayCreat.Length - 1] + "]");
+    Console.WriteLine();
+}
